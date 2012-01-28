2aec6ce01607cac466cfc1b1e5beb535120dfc65
Benedikt Böhm <bb@xnull.de>
2012-01-28 12:15:13 +0100

    reimport version used at madvertise

--- lib/slickgrid/rails/version.rb
+++ lib/slickgrid/rails/version.rb
@@ -1,6 +1,6 @@
 module SlickGrid
   module Rails
     VERSION = "0.1.0"
-    SLICK_VERSION = "42e35cbca5cbdf55b0bd6eb6881782bdfbc3f6a3"
+    SLICK_VERSION = "5f88203f4b3c70eed7feaf31e65d71d95d2272c5-custom"
   end
 end
