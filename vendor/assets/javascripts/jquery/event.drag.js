3a2c106a95ce2c09520f0e824bd93abb187e93b3
Benedikt Böhm <bb@xnull.de>
2012-02-03 10:40:26 +0100

    convert jquery.event.drag to unix line endings and UTF-8 ... sigh

--- vendor/assets/javascripts/jquery/event.drag.js
+++ vendor/assets/javascripts/jquery/event.drag.js
@@ -170,7 +170,7 @@ drag = $special.drag = {
 		switch ( event.type ){
 			// mousemove, check distance, start dragging
 			case !dd.dragging && 'mousemove': 
-				//  drag tolerance, x² + y² = distance²
+				//  drag tolerance, xÂ² + yÂ² = distanceÂ²
 				if ( Math.pow(  event.pageX-dd.pageX, 2 ) + Math.pow(  event.pageY-dd.pageY, 2 ) < Math.pow( dd.distance, 2 ) ) 
 					break; // distance tolerance not reached
 				event.target = dd.target; // force target from "mousedown" event (fix distance issue)
