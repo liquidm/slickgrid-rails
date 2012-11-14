2c5d16b28f888943f787d20a53858888238357a9
Woody Peterson <woody.peterson@gmail.com>
2012-11-14 15:05:14 -0800

    Fix checkmark formatter url
    Previously, the checkmark formatter returned the wrong image path.
    Ideally, we'd use an asset path helper, but that would require a
    bigger change, and this is good enough I think.

--- vendor/assets/javascripts/slick/formatters.js
+++ vendor/assets/javascripts/slick/formatters.js
@@ -50,6 +50,6 @@
   }
 
   function CheckmarkFormatter(row, cell, value, columnDef, dataContext) {
-    return value ? "<img src='../images/tick.png'>" : "";
+    return value ? "<img src='/assets/slick/tick.png'>" : "";
   }
 })(jQuery);
